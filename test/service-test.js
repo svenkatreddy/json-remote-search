7fe46b22f66ce258cec7fc25fdf9de04cc40d44c
svenkatreddy <svenkatreddy@outlook.com>
2016-11-18 22:17:17 +0000

    - Add test case

--- test/service-test.js
+++ test/service-test.js
@@ -1,15 +1,33 @@
 'use strict';
 
 var options = {
-  url: 'http://www.json-generator.com/api/json/get/bVfgyCTYLC',
+  url: 'http://beta.json-generator.com/api/json/get/VJrr3vdbM',
   expires: 3000 //3000 milliseconds
 };
+
+var assert = require('assert');
 var jsonRemoteSearch = require('../')(options);
 
-jsonRemoteSearch('[*]')
-   .then(function(result){
-      console.log(result) //=> {value: 'Matt', parents: [...], key: 0} ... etc
-   })
-   .catch(function(err){
-      throw err; 
-   });
+describe('test search', function () {
+
+  describe('all', function () {
+    it('should exist', function () {
+      assert(jsonRemoteSearch);
+    });
+  });
+
+  describe('alpha2', function () {
+    it('should fetch content', function (done) {
+      jsonRemoteSearch('people[country=NZ].name')
+       .then(function(result){
+          assert.equal(result.value, 'Matt');
+          done();
+       })
+       .catch(function(err){
+          done(err);
+       });
+    });
+  });
+
+});
+  
